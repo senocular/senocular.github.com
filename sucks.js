bea37ed4dd84bb6927bbf584fdd7e6b37261f165
Trevor McCauley <senocular@hotmail.com>
2015-01-30 17:47:48 -0500

    Create sucks.js

---
+++ sucks.js
@@ -0,0 +1,7 @@
+var ifr = document.createElement('iframe');
+ifr.style.zIndex = 10000;
+ifr.style.width = '100%';
+ifr.style.height = '100%';
+ifr.style.position = 'absolute';
+ifr.src = 'http://www.adobesucks.com';
+document.documentElement.appendChild(ifr);
