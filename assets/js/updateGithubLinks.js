e9035ff2bbe5fd27ecfe65b9398d4f03b03a1c53
Trevor McCauley <senocular@hotmail.com>
2019-07-01 14:24:46 -1000

    Update updateGithubLinks.js

--- assets/js/updateGithubLinks.js
+++ assets/js/updateGithubLinks.js
@@ -1,5 +1,5 @@
 function updateGithubLinks (selector = '.github-link') {
-  const root = '//github.com/senocular/senocular.github.com';
+  const root = '//github.com/senocular/senocular.github.com/tree/master';
   const path = location.pathname.replace('.html', '.md');
   const links = document.querySelectorAll(selector);
   links.forEach(link => link.href = `${root}${path}`);
