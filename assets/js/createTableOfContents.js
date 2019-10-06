85063515d93f79275dcb322547b4064d5129c9f8
Trevor McCauley <senocular@hotmail.com>
2019-10-06 13:38:41 -0400

    Create createTableOfContents.js

---
+++ assets/js/createTableOfContents.js
@@ -0,0 +1,58 @@
+
+function createTableOfContents () {
+  const toc = document.querySelector('.toc');
+  const content = document.querySelector('.markdown-body');
+  
+  if (!toc || !content) return;
+  
+  const headingsSelector = 'h1,h2,h3,h4,h5,h6';
+  const headingTags = headingsSelector.toUpperCase().split(',');
+  
+  const headings = content.querySelectorAll(headingsSelector);
+  if (!headings.length) {
+    doc.style.display = 'none';
+    return;
+  }
+  
+  const panel = toc.querySelector('.toc-panel');
+  
+  let listStack = [];
+  headings.forEach(heading => {
+    const tagIndex = headingTags.indexOf(heading.tagName);
+    if (tagIndex < 0) return;
+    
+    if (tagIndex < listStack.length) {
+      listStack.length = tagIndex;
+    }
+    
+    let list;
+    while (!(list = listStack[tagIndex])) {
+      list = document.createElement('ul');
+      const parent = listStack.length
+        ? listStack[listStack.length - 1].lastElementChild
+        : panel;
+      parent.appendChild(list);
+      listStack.push(list);
+    }
+    
+    const item = document.createElement('li');
+    const link = document.createElement('a');
+    link.textContent = heading.textContent;
+    link.href = '#' + heading.id;
+    item.appendChild(link);
+    list.appendChild(item);
+  });
+  
+  window.addEventListener('click', event => {
+    if (toc.contains(event.target)) {
+      let tag = event.target.tagName;
+      if (tag === 'BUTTON') {
+        toc.classList.toggle('expanded');
+      } else if (tag === 'A') {
+        toc.classList.remove('expanded');
+      }
+    } else {
+      toc.classList.remove('expanded');
+    }
+  });
+}
