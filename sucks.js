47f72f746b13154a14743bbe1f77f40a9e978348
Trevor McCauley <3536716+senocular@users.noreply.github.com>
2024-04-20 12:40:43 -0400

    Delete sucks.js

--- sucks.js
+++
@@ -1,7 +0,0 @@
-var ifr = document.createElement('iframe');
-ifr.style.zIndex = 10000;
-ifr.style.width = '100%';
-ifr.style.height = '100%';
-ifr.style.position = 'absolute';
-ifr.src = 'https://senocular.github.io/meow.html';
-document.documentElement.appendChild(ifr);
